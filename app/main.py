d8f75e9b01fd8e8810f48c889843e17c8d14b699
Mihail <kanevskymihail@gmail.com>
2026-05-19 17:09:19 +0200

    Add lead detail endpoint

--- app/main.py
+++ app/main.py
@@ -47,3 +47,16 @@ def create_lead(lead_data: LeadCreate, db: Session = Depends(get_db)):
 def get_leads(db: Session = Depends(get_db)):
     leads = db.query(Lead).order_by(Lead.id).all()
     return leads
+
+
+@app.get("/leads/{lead_id}", response_model=LeadRead)
+def get_lead(lead_id: int, db: Session = Depends(get_db)):
+    lead = db.query(Lead).filter(Lead.id == lead_id).first()
+
+    if not lead:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Lead with id {lead_id} doesn't exist"
+        )
+
+    return lead
